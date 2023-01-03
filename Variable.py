a4c5218c38b1178d2a30aa5d322863662cbfae10
Angel Alvarado <Angelmad2018huawei@gmail.com>
2023-01-03 00:00:15 -0500

    Creation file Variable.py

---
+++ Variable.py
@@ -0,0 +1,68 @@
+# En este archivo py veremos las variables
+# Para empezar, las variables como yo las conozco son
+# un espacio que la memoria reserva para almacenar valores
+# o datos que nos permitirán realizar distintos
+# tipos de operaciones o manipularlas.
+# Dicho de otra forma, usamos las variables como cajas
+# donde almacenamos lo que queremos, puede ser
+# una cadena, un número, un valor booelano o una estructura
+# mas grande (tupla, listas, diccionarios, etc)
+
+# VARIABLES
+
+print("Variables")
+
+Saludo = "Hola, bienvenido a la escuela de Python"
+# "Saludo" es una variable (espacio en memoria) que guarda el mensaje que está
+# a la derecha del igual "="
+print(type(Saludo))  # Imprime el tipo de dato que contiene la variable
+print(Saludo)    # Imprime el contenido de la variable
+
+opcioBool = False
+print(opcioBool)  # Imprime False en consola
+
+numEntero = 15
+numDecimal = 15.12
+print(type(numEntero))  # int
+print(type(numDecimal)) # float
+print(numEntero)   # 15
+print(numDecimal)  # 15.12
+
+# Concatenar e imprimir las variables en formato string
+# Forma válida 1, la forma mas utilizada para imprimir datos aunque 
+# no por eso la única
+print(Saludo, opcioBool, numDecimal, numEntero)
+# Forma válida 2, en este caso se hace una conversion a string
+# de las variables boolenas y numéricas, en el anterior caso, la conversion
+# ya viene implícita
+print(Saludo + str(opcioBool) + str(numDecimal) + str(numEntero))
+
+# Probaré algunas funciones del sistema de Python
+print(len(Saludo))
+print(pow(12, 3))
+print(abs(-123))
+
+# Podemos crear e inicializar variables en una sola línea
+nombre, apellido, edad, hombre = "Angel", "Alvarado", 25, True
+# la anterior forma no es recomendada, así que no la usaremos mucho
+
+print("Me llamo", nombre, apellido, " tengo", edad, "años")
+
+# Python al ser un lenguaje debilmente tipado, nos permite cambiar los tipos
+# de las variables previamente definidas
+
+nombre = 15
+apellido = False
+hombre = 56.2
+
+print(nombre, apellido, hombre)
+
+# Entrada de datos por consola
+# input, este método nos permite ingresar datos por consola con el teclado
+# sin embargo, dicen que no es muy utilizado
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+edad = input("Ingresa tu edad: ")
+
+print("Tu nombre es:", nombre, apellido, "y tienes", edad, "años")
+
--- holaMundo.py
+++ holaMundo.py
@@ -1,3 +1,21 @@
 # Primer comentario
 # Imprimir "Hola Mundo Querido, hola a todos" por consola
 print("Hola Mundo Querido, hola a todos")
+
+"""Este es un comentario de varias 
+líneas
+Debo ser honesto, esta es como la quinta vez que veo
+python, pero a diferencia
+de las cuatro anteriores veces, en esta
+guardaré los cambias en un repositorio en la nube, 
+en este caso git hub"""
+
+# Averiguar el tipo de dato con el método type()
+print(type(15.1))           # este número es un float  (flotante)
+print(type("hola mundo"))   # es un str  (cadena)
+print(type(True))           # es un bool  (booleano)
+print(type(1200))           # es un int  (entero)
+print(type('A'))            # es un str  (cadena)
+
+
+
